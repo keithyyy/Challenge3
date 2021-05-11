f9681ce2b6d02dc200d26356ec56e300c81b757e
Keith Crooc <keith.crooc@gmail.com>
2021-05-10 21:41:00 -0400

    Setting up labels and layout

--- Challenge3/ViewController.swift
+++ Challenge3/ViewController.swift
@@ -8,12 +8,70 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    
+    var wordOptions = ["RHYTHM", "AWESOME", "TWICE", "CHARGE"]
+    var word = ""
+    
+    
+    var scoreLabel: UILabel!
+    var chosenWord: UILabel!
+    var userInput: UITextField!
+    
+    
+    
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
     }
 
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+        
+        scoreLabel = UILabel()
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        scoreLabel.text = "Score: 0"
+        view.addSubview(scoreLabel)
+        
+        
+        chosenWord = UILabel()
+        chosenWord.translatesAutoresizingMaskIntoConstraints = false
+        chosenWord.font = UIFont.systemFont(ofSize: 24)
+        view.addSubview(chosenWord)
+        
+        
+        
+        
+        
+        NSLayoutConstraint.activate([
+        
+            scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            scoreLabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            
+            
+            
+            chosenWord.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor),
+            
+            
+            
+            
+        
+        
+        
+        
+        
+        ])
+    }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
 
 }
 
